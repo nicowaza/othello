0f966f46e155628b9f2cee9cb224c67e925a0c68
nicolas <nicolas.ducteil@omnilive.fr>
2019-03-01 14:56:39 +0100

    prod

---
+++ build/precache-manifest.0728c1e62cf5cc885b9172435574b38f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f92e390fcebc84fe9f8a",
+    "url": "/static/js/main.f92e390f.chunk.js"
+  },
+  {
+    "revision": "7ccb9e808ce602fa7deb",
+    "url": "/static/js/2.7ccb9e80.chunk.js"
+  },
+  {
+    "revision": "f92e390fcebc84fe9f8a",
+    "url": "/static/css/main.fa74bebe.chunk.css"
+  },
+  {
+    "revision": "48d002bfd0c2aa1a0eded32683cc4dc1",
+    "url": "/index.html"
+  }
+];
